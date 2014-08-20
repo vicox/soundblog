cf53e77b8825e352b64e3bbaea6f3764cdd0fca7
Georg Schmidl <georg.schmidl@vicox.net>
2014-08-20 21:20:17 +0200

    ability to create a post

--- js/admin.js
+++ js/admin.js
@@ -165,13 +165,85 @@
         }
     });
 
+    var PostCreateView = Backbone.View.extend({
+        el: '.github',
+        events: {
+            'submit .create-post': 'submit',
+            'submit .docreate-post': 'create'
+        },
+        submit: function (e) {
+            e.preventDefault();
+
+            var $form = $(e.currentTarget);
+            disableForm($form);
+
+            var url = $form.find('#url').val();
+
+            SC.initialize({
+                client_id: this.$el.data('soundcloud-client-id')
+            });
+
+            var that = this;
+            SC.get("/resolve", {url: url}, function(track){
+                var template = _.template($('#post-docreate-template').html(), {
+                    title: track.title,
+                    date: formatLocalDate(),
+                    id: track.id,
+                    kind: track.kind,
+                    permalink: track.user.permalink + '-' + track.permalink,
+                    url: url
+                });
+                that.$el.html(template);
+            });
+
+
+        },
+        create: function (e) {
+            e.preventDefault();
+
+            var $form = $(e.currentTarget);
+            disableForm($form);
+
+            var title = $form.find('#title').val();
+            var date = $form.find('#date').val();
+            var id = $form.find('#id').val();
+            var kind = $form.find('#kind').val();
+            var url = $form.find('#url').val();
+
+            var map = {};
+            map['layout'] = 'post';
+            map['title'] = '"' + title + '"';
+            map['date'] = date;
+            map['track_id'] = id;
+            if ('track' != kind) {
+                map['track_kind'] = kind;
+
+            }
+            map['track_url'] = url;
+
+            var dayFromDate = date.substring(0, 10);
+            var fileName = dayFromDate + '-' + $form.find('#permalink').val() + '.md';
+
+            branch.write('_posts/' + fileName, mapToContent(map), 'new post', false)
+                .then(function() {
+                    window.location.hash = '#/show/' + fileName;
+                });
+
+
+        },
+        render: function () {
+            this.$el.html($('#post-create-template').html());
+        }
+    });
+
     var Router = Backbone.Router.extend({
         routes: {
             "login": "login",
             "": "posts",
             "show/:fileName": "show",
             "edit/:fileName": "edit",
-            "delete/:fileName": "delete"
+            "delete/:fileName": "delete",
+            "create": "create"
         }
     });
 
@@ -190,6 +262,10 @@
     });
     router.on('route:delete', function (fileName) {
         new PostDeleteView().render({fileName: fileName});
+    });
+
+    router.on('route:create', function () {
+        new PostCreateView().render();
     });
 
     Backbone.history.start();
@@ -243,6 +319,24 @@
         $form.find('.error').remove();
         $form.prepend(_.template($('#error-template').html(), {error: error}));
         enableForm($form);
+    }
+
+    function pad(num) {
+        norm = Math.abs(Math.floor(num));
+        return (norm < 10 ? '0' : '') + norm;
+    }
+
+    function formatLocalDate() {
+        var local = new Date();
+        var tzo = -local.getTimezoneOffset();
+        var sign = tzo >= 0 ? '+' : '-';
+        return local.getFullYear()
+            + '-' + pad(local.getMonth()+1)
+            + '-' + pad(local.getDate())
+            + ' ' + pad(local.getHours())
+            + ':' + pad(local.getMinutes())
+            + ':' + pad(local.getSeconds())
+            + ' ' +sign + pad(tzo / 60) + pad(tzo % 60);
     }
 
 })();
